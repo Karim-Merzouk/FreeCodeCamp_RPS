35b1d22348949702c7321986f759a1eaba04449a
Karim-Merzouk <offsmrk@gmail.com>
2025-01-31 23:44:55 +0100

    RPS.py

--- RPS.py
+++ RPS.py
@@ -1,10 +1,26 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
+from collections import defaultdict
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    # Initialize transition probabilities
+    transition_matrix = defaultdict(lambda: {"R": 0, "P": 0, "S": 0})
+    
+    # Populate transition matrix based on history
+    for i in range(len(opponent_history) - 1):
+        current_move = opponent_history[i]
+        next_move = opponent_history[i + 1]
+        transition_matrix[current_move][next_move] += 1
+    
+    # Predict the next move
+    if len(opponent_history) > 1:
+        last_move = opponent_history[-1]
+        predicted_move = max(transition_matrix[last_move], key=transition_matrix[last_move].get)
+    else:
+        predicted_move = random.choice(["R", "P", "S"])
+    
+    # Counter the predicted move
+    ideal_response = {"R": "P", "P": "S", "S": "R"}
+    return ideal_response[predicted_move]
